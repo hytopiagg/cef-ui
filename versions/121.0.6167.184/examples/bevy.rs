abadad8d273c936ca945709fc7ec0af7727da694
wusticality <1028792+wusticality@users.noreply.github.com>
2024-02-22 16:38:50 -0700

    Switch to using versioned releases; add Linux build script

--- versions/121.0.6167.184/examples/bevy.rs
+++ versions/121.0.6167.184/examples/bevy.rs
@@ -1,9 +1,6 @@
 use bevy::{app::App, prelude::Update, window::close_on_esc, DefaultPlugins};
-use cef_ui::bindings::cef_settings_t;
 
 fn main() {
-    let settings = cef_settings_t {};
-
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Update, close_on_esc)
