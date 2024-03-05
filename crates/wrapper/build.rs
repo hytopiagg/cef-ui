95e426a0f721bfa17d9f9b37c9800a453786e1f1
wusticality <1028792+wusticality@users.noreply.github.com>
2024-03-04 17:01:25 -0700

    CEF is running for the first time!

--- crates/wrapper/build.rs
+++ crates/wrapper/build.rs
@@ -4,14 +4,12 @@ use std::env;
 fn main() -> Result<()> {
     let cef_dir = env::var("CEF_DIR")?;
 
-    println!("cargo:warning=CEF_DIR: {}", cef_dir);
-
     // This tells Rust how to link against libcef.so.
     println!("cargo:rustc-link-search=native={}", cef_dir);
     println!("cargo:rustc-link-lib=dylib=cef");
 
     // This tells Rust how to find libcef.so at runtime.
-    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/cef/Release");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN/cef");
 
     Ok(())
 }
