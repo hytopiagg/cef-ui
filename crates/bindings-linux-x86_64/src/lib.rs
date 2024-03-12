d0c7fe71a78bdb5b416268d9264b440cff685570
wusticality <1028792+wusticality@users.noreply.github.com>
2024-03-12 12:49:31 -0600

    Add missing CEF #define's

--- crates/bindings-linux-x86_64/src/lib.rs
+++ crates/bindings-linux-x86_64/src/lib.rs
@@ -4,4 +4,11 @@
 #[allow(non_snake_case)]
 mod bindings;
 
+// Ignore these warnings.
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+mod defines;
+
 pub use bindings::*;
+pub use defines::*;
