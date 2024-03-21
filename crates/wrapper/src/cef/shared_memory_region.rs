d0d2f36d35512f551aca22348bafb3c6ace82ddf
wusticality <1028792+wusticality@users.noreply.github.com>
2024-03-21 11:46:26 -0600

    Safe C functions for shared_memory_region.rs

--- crates/wrapper/src/cef/shared_memory_region.rs
+++ crates/wrapper/src/cef/shared_memory_region.rs
@@ -1,33 +1,25 @@
-use crate::ref_counted_ptr;
+use crate::{ref_counted_ptr, try_c};
+use anyhow::Result;
 use bindings::cef_shared_memory_region_t;
-use std::{ffi::c_void, ptr::null_mut};
+use std::ffi::c_void;
 
 // Structure that wraps platform-dependent share memory region mapping.
 ref_counted_ptr!(SharedMemoryRegion, cef_shared_memory_region_t);
 
 impl SharedMemoryRegion {
     /// Returns true (1) if the mapping is valid.
-    pub fn is_valid(&self) -> bool {
-        self.0
-            .is_valid
-            .map(|is_valid| unsafe { is_valid(self.as_ptr()) != 0 })
-            .unwrap_or(false)
+    pub fn is_valid(&self) -> Result<bool> {
+        try_c!(self, is_valid, { Ok(is_valid(self.as_ptr()) != 0) })
     }
 
     /// Returns the size of the mapping in bytes. Returns 0 for invalid instances.
-    pub fn size(&self) -> usize {
-        self.0
-            .size
-            .map(|size| unsafe { size(self.as_ptr()) })
-            .unwrap_or(0)
+    pub fn size(&self) -> Result<usize> {
+        try_c!(self, size, { Ok(size(self.as_ptr())) })
     }
 
     /// Returns the pointer to the memory. Returns nullptr for invalid instances.
     /// The returned pointer is only valid for the life span of this object.
-    pub fn memory(&self) -> *mut c_void {
-        self.0
-            .memory
-            .map(|data| unsafe { data(self.as_ptr()) })
-            .unwrap_or(null_mut())
+    pub fn memory(&self) -> Result<*mut c_void> {
+        try_c!(self, memory, { Ok(memory(self.as_ptr())) })
     }
 }
