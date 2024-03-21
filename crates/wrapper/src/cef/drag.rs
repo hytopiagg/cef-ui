b88a4800adc48033a07a76ee93ffaed0ca488cc7
wusticality <1028792+wusticality@users.noreply.github.com>
2024-03-21 09:43:59 -0600

    Safe C functions for drag.rs

--- crates/wrapper/src/cef/drag.rs
+++ crates/wrapper/src/cef/drag.rs
@@ -1,4 +1,5 @@
-use crate::{ref_counted_ptr, CefString, CefStringList, Point};
+use crate::{ref_counted_ptr, try_c, CefString, CefStringList, Point};
+use anyhow::Result;
 use bindings::{
     cef_drag_data_create, cef_drag_data_t, cef_drag_operations_mask_t,
     cef_drag_operations_mask_t_DRAG_OPERATION_COPY,
@@ -64,120 +65,94 @@ impl DragData {
     }
 
     /// Returns a copy of the current object.
-    pub fn copy(&self) -> Option<DragData> {
-        self.0
-            .clone
-            .map(|clone| unsafe { Self::from_ptr_unchecked(clone(self.as_ptr())) })
+    pub fn copy(&self) -> Result<DragData> {
+        try_c!(self, clone, {
+            Ok(Self::from_ptr_unchecked(clone(self.as_ptr())))
+        })
     }
 
     /// Returns true (1) if this object is read-only.
-    pub fn is_read_only(&self) -> bool {
-        self.0
-            .is_read_only
-            .map(|is_read_only| unsafe { is_read_only(self.as_ptr()) != 0 })
-            .unwrap_or(true)
+    pub fn is_read_only(&self) -> Result<bool> {
+        try_c!(self, is_read_only, { Ok(is_read_only(self.as_ptr()) != 0) })
     }
 
     /// Returns true (1) if the drag data is a link.
-    pub fn is_link(&self) -> bool {
-        self.0
-            .is_link
-            .map(|is_link| unsafe { is_link(self.as_ptr()) != 0 })
-            .unwrap_or(false)
+    pub fn is_link(&self) -> Result<bool> {
+        try_c!(self, is_link, { Ok(is_link(self.as_ptr()) != 0) })
     }
 
     /// Returns true (1) if the drag data is a text or html fragment.
-    pub fn is_fragment(&self) -> bool {
-        self.0
-            .is_fragment
-            .map(|is_fragment| unsafe { is_fragment(self.as_ptr()) != 0 })
-            .unwrap_or(false)
+    pub fn is_fragment(&self) -> Result<bool> {
+        try_c!(self, is_fragment, { Ok(is_fragment(self.as_ptr()) != 0) })
     }
 
     /// Returns true (1) if the drag data is a file.
-    pub fn is_file(&self) -> bool {
-        self.0
-            .is_file
-            .map(|is_file| unsafe { is_file(self.as_ptr()) != 0 })
-            .unwrap_or(false)
+    pub fn is_file(&self) -> Result<bool> {
+        try_c!(self, is_file, { Ok(is_file(self.as_ptr()) != 0) })
     }
 
     /// Return the link URL that is being dragged.
-    pub fn get_link_url(&self) -> Option<String> {
-        self.0
-            .get_link_url
-            .map(|get_link_url| {
-                let s = unsafe { get_link_url(self.as_ptr()) };
+    pub fn get_link_url(&self) -> Result<String> {
+        try_c!(self, get_link_url, {
+            let s = get_link_url(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     /// Return the title associated with the link being dragged.
-    pub fn get_link_title(&self) -> Option<String> {
-        self.0
-            .get_link_title
-            .map(|get_link_title| {
-                let s = unsafe { get_link_title(self.as_ptr()) };
+    pub fn get_link_title(&self) -> Result<String> {
+        try_c!(self, get_link_title, {
+            let s = get_link_title(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     /// Return the metadata, if any, associated with the link being dragged.
-    pub fn get_link_metadata(&self) -> Option<String> {
-        self.0
-            .get_link_metadata
-            .map(|get_link_metadata| {
-                let s = unsafe { get_link_metadata(self.as_ptr()) };
+    pub fn get_link_metadata(&self) -> Result<String> {
+        try_c!(self, get_link_metadata, {
+            let s = get_link_metadata(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     /// Return the plain text fragment that is being dragged.
-    pub fn get_fragment_text(&self) -> Option<String> {
-        self.0
-            .get_fragment_text
-            .map(|get_fragment_text| {
-                let s = unsafe { get_fragment_text(self.as_ptr()) };
+    pub fn get_fragment_text(&self) -> Result<String> {
+        try_c!(self, get_fragment_text, {
+            let s = get_fragment_text(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     /// Return the text/html fragment that is being dragged.
-    pub fn get_fragment_html(&self) -> Option<String> {
-        self.0
-            .get_fragment_html
-            .map(|get_fragment_html| {
-                let s = unsafe { get_fragment_html(self.as_ptr()) };
+    pub fn get_fragment_html(&self) -> Result<String> {
+        try_c!(self, get_fragment_html, {
+            let s = get_fragment_html(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     /// Return the base URL that the fragment came from. This value is used for
     /// resolving relative URLs and may be NULL.
-    pub fn get_fragment_base_url(&self) -> Option<String> {
-        self.0
-            .get_fragment_base_url
-            .map(|get_fragment_base_url| {
-                let s = unsafe { get_fragment_base_url(self.as_ptr()) };
+    pub fn get_fragment_base_url(&self) -> Result<Option<String>> {
+        try_c!(self, get_fragment_base_url, {
+            let s = get_fragment_base_url(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr(s).map(|s| s.into()))
+        })
     }
 
     /// Return the name of the file being dragged out of the browser window.
-    pub fn get_file_name(&self) -> Option<String> {
-        self.0
-            .get_file_name
-            .map(|get_file_name| {
-                let s = unsafe { get_file_name(self.as_ptr()) };
+    pub fn get_file_name(&self) -> Result<String> {
+        try_c!(self, get_file_name, {
+            let s = get_file_name(self.as_ptr());
 
-                CefString::from_userfree_ptr_unchecked(s).into()
-            })
+            Ok(CefString::from_userfree_ptr_unchecked(s).into())
+        })
     }
 
     // TODO: Fix this!
@@ -193,134 +168,124 @@ impl DragData {
 
     /// Retrieve the list of file names that are being dragged into the browser
     /// window.
-    pub fn get_file_names(&self) -> Vec<String> {
-        self.0
-            .get_file_names
-            .map(|get_file_names| {
-                let mut list = CefStringList::new();
+    pub fn get_file_names(&self) -> Result<Vec<String>> {
+        try_c!(self, get_file_names, {
+            let mut list = CefStringList::new();
 
-                unsafe {
-                    get_file_names(self.as_ptr(), list.as_mut_ptr());
-                }
+            get_file_names(self.as_ptr(), list.as_mut_ptr());
 
-                list.into()
-            })
-            .unwrap_or_default()
+            Ok(list.into())
+        })
     }
 
     /// Retrieve the list of file paths that are being dragged into the browser
     /// window.
-    pub fn get_file_paths(&self) -> Vec<String> {
-        self.0
-            .get_file_paths
-            .map(|get_file_paths| {
-                let mut list = CefStringList::new();
+    pub fn get_file_paths(&self) -> Result<Vec<String>> {
+        try_c!(self, get_file_paths, {
+            let mut list = CefStringList::new();
 
-                unsafe {
-                    get_file_paths(self.as_ptr(), list.as_mut_ptr());
-                }
+            get_file_paths(self.as_ptr(), list.as_mut_ptr());
 
-                list.into()
-            })
-            .unwrap_or_default()
+            Ok(list.into())
+        })
     }
 
     /// Set the link URL that is being dragged.
-    pub fn set_link_url(&self, url: &str) {
-        if let Some(set_link_url) = self.0.set_link_url {
+    pub fn set_link_url(&self, url: &str) -> Result<()> {
+        try_c!(self, set_link_url, {
             let url = CefString::new(url);
 
-            unsafe {
-                set_link_url(self.as_ptr(), url.as_ptr());
-            }
-        }
+            set_link_url(self.as_ptr(), url.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Set the title associated with the link being dragged.
-    pub fn set_link_title(&self, title: &str) {
-        if let Some(set_link_title) = self.0.set_link_title {
+    pub fn set_link_title(&self, title: &str) -> Result<()> {
+        try_c!(self, set_link_title, {
             let title = CefString::new(title);
 
-            unsafe {
-                set_link_title(self.as_ptr(), title.as_ptr());
-            }
-        }
+            set_link_title(self.as_ptr(), title.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Set the metadata associated with the link being dragged.
-    pub fn set_link_metadata(&self, data: &str) {
-        if let Some(set_link_metadata) = self.0.set_link_metadata {
+    pub fn set_link_metadata(&self, data: &str) -> Result<()> {
+        try_c!(self, set_link_metadata, {
             let data = CefString::new(data);
 
-            unsafe {
-                set_link_metadata(self.as_ptr(), data.as_ptr());
-            }
-        }
+            set_link_metadata(self.as_ptr(), data.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Set the plain text fragment that is being dragged.
-    pub fn set_fragment_text(&self, text: &str) {
-        if let Some(set_fragment_text) = self.0.set_fragment_text {
+    pub fn set_fragment_text(&self, text: &str) -> Result<()> {
+        try_c!(self, set_fragment_text, {
             let text = CefString::new(text);
 
-            unsafe {
-                set_fragment_text(self.as_ptr(), text.as_ptr());
-            }
-        }
+            set_fragment_text(self.as_ptr(), text.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Set the text/html fragment that is being dragged.
-    pub fn set_fragment_html(&self, html: &str) {
-        if let Some(set_fragment_html) = self.0.set_fragment_html {
+    pub fn set_fragment_html(&self, html: &str) -> Result<()> {
+        try_c!(self, set_fragment_html, {
             let html = CefString::new(html);
 
-            unsafe {
-                set_fragment_html(self.as_ptr(), html.as_ptr());
-            }
-        }
+            set_fragment_html(self.as_ptr(), html.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Set the base URL that the fragment came from.
-    pub fn set_fragment_base_url(&self, base_url: &str) {
-        if let Some(set_fragment_base_url) = self.0.set_fragment_base_url {
+    pub fn set_fragment_base_url(&self, base_url: &str) -> Result<()> {
+        try_c!(self, set_fragment_base_url, {
             let base_url = CefString::new(base_url);
 
-            unsafe {
-                set_fragment_base_url(self.as_ptr(), base_url.as_ptr());
-            }
-        }
+            set_fragment_base_url(self.as_ptr(), base_url.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Reset the file contents. You should do this before calling
     /// cef_browser_host_t::DragTargetDragEnter as the web view does not allow us
     /// to drag in this kind of data.
-    pub fn reset_file_contents(&self) {
-        if let Some(reset_file_contents) = self.0.reset_file_contents {
-            unsafe {
-                reset_file_contents(self.as_ptr());
-            }
-        }
+    pub fn reset_file_contents(&self) -> Result<()> {
+        try_c!(self, reset_file_contents, {
+            reset_file_contents(self.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Add a file that is being dragged into the webview.
-    pub fn add_file(&self, path: &str, display_name: &str) {
-        if let Some(add_file) = self.0.add_file {
+    pub fn add_file(&self, path: &str, display_name: &str) -> Result<()> {
+        try_c!(self, add_file, {
             let path = CefString::new(path);
             let display_name = CefString::new(display_name);
 
-            unsafe {
-                add_file(self.as_ptr(), path.as_ptr(), display_name.as_ptr());
-            }
-        }
+            add_file(self.as_ptr(), path.as_ptr(), display_name.as_ptr());
+
+            Ok(())
+        })
     }
 
     /// Clear list of filenames.
-    pub fn clear_filenames(&self) {
-        if let Some(clear_filenames) = self.0.clear_filenames {
-            unsafe {
-                clear_filenames(self.as_ptr());
-            }
-        }
+    pub fn clear_filenames(&self) -> Result<()> {
+        try_c!(self, clear_filenames, {
+            clear_filenames(self.as_ptr());
+
+            Ok(())
+        })
     }
 
     // TODO: Fix this!
@@ -332,17 +297,14 @@ impl DragData {
     //     struct _cef_image_t*(CEF_CALLBACK* get_image)(struct _cef_drag_data_t* self);
 
     /// Get the image hotspot (drag start location relative to image dimensions).
-    pub fn get_image_hotspot(&self) -> Option<Point> {
-        self.0
-            .get_image_hotspot
-            .map(|get_image_hotspot| unsafe { get_image_hotspot(self.as_ptr()).into() })
+    pub fn get_image_hotspot(&self) -> Result<Point> {
+        try_c!(self, get_image_hotspot, {
+            Ok(get_image_hotspot(self.as_ptr()).into())
+        })
     }
 
     /// Returns true (1) if an image representation of drag data is available.
-    pub fn has_image(&self) -> bool {
-        self.0
-            .has_image
-            .map(|has_image| unsafe { has_image(self.as_ptr()) != 0 })
-            .unwrap_or(false)
+    pub fn has_image(&self) -> Result<bool> {
+        try_c!(self, has_image, { Ok(has_image(self.as_ptr()) != 0) })
     }
 }
