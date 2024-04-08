a86202a8551541373a2b9d33ac008240a0292f62
wusticality <1028792+wusticality@users.noreply.github.com>
2024-04-08 16:03:43 -0600

    Stubs for the simple helper binary crate

--- crates/cef-ui-simple-helper/src/main.rs
+++ crates/cef-ui-simple-helper/src/main.rs
@@ -1,1 +1,13 @@
-fn main() {}
+use std::process::exit;
+
+fn main() {
+    if let Err(e) = try_main() {
+        eprintln!("Error: {}", e);
+
+        exit(1);
+    }
+}
+
+fn try_main() -> anyhow::Result<()> {
+    Ok(())
+}
