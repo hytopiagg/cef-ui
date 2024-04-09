1dd6e59b9bfc65cd33e940ea3271ea5bff082f67
wusticality <1028792+wusticality@users.noreply.github.com>
2024-04-09 15:14:14 -0600

    Share imports at the workspace level

--- crates/cef-ui-simple-helper/src/main.rs
+++ crates/cef-ui-simple-helper/src/main.rs
@@ -1,16 +1,15 @@
 use crate::main_args::{cef_main_args_t, MainArgs};
 use anyhow::{anyhow, Result};
 use libloading::{Library, Symbol};
-use log::{debug, error};
-use rand::{prelude::StdRng, Rng, SeedableRng};
+use log::{error, info};
 use std::{
     env,
+    env::current_exe,
     ffi::{c_int, c_void},
-    fs::{canonicalize, File},
+    fs::canonicalize,
     path::PathBuf,
     process::exit,
-    ptr::null_mut,
-    time::{SystemTime, UNIX_EPOCH}
+    ptr::null_mut
 };
 use tracing::{level_filters::LevelFilter, subscriber::set_global_default, Level};
 use tracing_log::LogTracer;
@@ -23,10 +22,12 @@ const CEF_PATH: &str = "../../../Chromium Embedded Framework.framework/Chromium 
 
 fn main() {
     let ret = try_main().unwrap_or_else(|e| {
-        eprintln!("Error: {}", e);
+        error!("An error occurred: {}", e);
 
         1
     });
+
+    info!("The return code is: {}", ret);
 
     exit(ret);
 }
@@ -35,83 +36,70 @@ fn try_main() -> Result<i32> {
     // This routes log macros through tracing.
     LogTracer::init()?;
 
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-
-    // Use the duration's whole seconds as the seed
-    let seed = since_the_epoch.as_secs();
-
-    // Create a seeded RNG
-    let mut rng = StdRng::seed_from_u64(seed);
-
-    // Generate a random number
-    let random_number: i32 = rng.gen();
-
-    let filename = format!("/Users/kevin/repos/cef-ui/HELPER-{}.log", random_number);
-
-    // Open a file to write logs to
-    let log_file = File::create(filename)?;
+    // Open a file to emit logs to.
+    // let filename = get_log_path(&PathBuf::from("/Users/kevin/repos/cef-ui"))?;
+    // let log_file = File::create(filename)?;
 
     // Setup the tracing subscriber globally.
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(LevelFilter::from_level(Level::TRACE))
-        .with_writer(log_file)
+        .with_max_level(LevelFilter::from_level(Level::DEBUG))
+        // .with_writer(log_file)
         .finish();
 
     set_global_default(subscriber)?;
 
     let cef_path = get_cef_path(CEF_PATH)?;
 
-    debug!("cef_path: {:?}", cef_path);
+    info!("cef_path: {:?}", cef_path);
 
     let main_args = MainArgs::new(env::args())?;
 
-    debug!("main_args: {:?}", main_args);
+    info!("main_args: {:?}", main_args);
 
     let ret = unsafe {
         let lib = Library::new(cef_path)?;
 
         let cef_execute_process: Symbol<
             unsafe extern "C" fn(args: *const cef_main_args_t, *mut c_void, *mut c_void) -> c_int
-        > = lib
-            .get(b"cef_execute_process")
-            .unwrap_or_else(|e| {
-                debug!("Could not get cef_execute_process: {:?}", e);
-
-                exit(1);
-            });
-
-        debug!("loaded cef_execute_process");
+        > = lib.get(b"cef_execute_process")?;
 
         let ret = cef_execute_process(main_args.as_raw(), null_mut(), null_mut()) as i32;
 
-        debug!("cef_execute_process returned: {}", ret);
-
-        if let Err(e) = lib.close() {
-            error!("Could not close library: {:?}", e);
-        }
+        lib.close()?;
 
         ret
     };
 
-    debug!("cef_execute_process returned: {}", ret);
-
     Ok(ret)
 }
 
-/// Get the directory of the current executable.
-fn get_exe_dir() -> Result<PathBuf> {
-    env::current_exe()?
-        .parent()
-        .map(PathBuf::from)
-        .ok_or_else(|| anyhow!("Could not get parent directory"))
+/// Convert the exe name to something sane.
+#[allow(dead_code)]
+fn get_log_path(base: &PathBuf) -> Result<PathBuf> {
+    let name = current_exe()?;
+    let name = name
+        .file_stem()
+        .ok_or_else(|| anyhow!("Could not get exe name."))?;
+    let name = name
+        .to_str()
+        .ok_or_else(|| anyhow!("Could not convert exe name to string."))?;
+    let name = name
+        .replace(" ", "")
+        .replace("(", "")
+        .replace(")", "");
+    let name = format!("HELPER-{}.log", name);
+    let path = base.join(name);
+
+    Ok(path)
 }
 
 /// Get the cef library path.
 fn get_cef_path(relative_path: &str) -> Result<PathBuf> {
-    let cef_path = get_exe_dir()?.join(relative_path);
+    let cef_path = current_exe()?
+        .parent()
+        .map(PathBuf::from)
+        .ok_or_else(|| anyhow!("Could not get parent directory"))?;
+    let cef_path = cef_path.join(relative_path);
     let cef_path = canonicalize(cef_path)?;
 
     Ok(cef_path)
