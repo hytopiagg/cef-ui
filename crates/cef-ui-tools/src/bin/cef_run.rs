18446939cb684c9fa83b9ca4f56f8dee7f6fd9c5
wusticality <1028792+wusticality@users.noreply.github.com>
2024-04-23 05:21:39 -0600

    Remove cef-run binary targets

--- crates/cef-ui-tools/src/bin/cef_run.rs
+++
@@ -1,8 +0,0 @@
-use anyhow::Result;
-use cef_ui_tools::cef_run;
-
-fn main() -> Result<()> {
-    cef_run()?;
-
-    Ok(())
-}
--- crates/cef-ui-tools/src/lib.rs
+++ crates/cef-ui-tools/src/lib.rs
@@ -1,11 +1,9 @@
 mod cef_artifacts;
 mod cef_build;
 mod cef_clean;
-mod cef_run;
 mod util;
 
 pub use cef_artifacts::*;
 pub use cef_build::*;
 pub use cef_clean::*;
-pub use cef_run::*;
 pub use util::*;
