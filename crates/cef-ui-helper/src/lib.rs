e7c1f0d4e2707340114e3e35084d75013e4ee522
wusticality <1028792+wusticality@users.noreply.github.com>
2024-05-01 02:50:33 -0600

    Add comments

--- crates/cef-ui-helper/src/lib.rs
+++ crates/cef-ui-helper/src/lib.rs
@@ -64,19 +64,23 @@ fn try_run() -> Result<i32> {
 
         info!("Main args: {:?}", main_args);
 
-        // Manually load the cef_execute_process function.
+        // Manually load the CEF framework.
         let cef_path = get_cef_path(CEF_PATH)?;
         let lib = Library::new(cef_path)?;
+
+        // Manually load the cef_execute_process function.
         let cef_execute_process: Symbol<
             unsafe extern "C" fn(args: *const cef_main_args_t, *mut c_void, *mut c_void) -> c_int
         > = lib.get(b"cef_execute_process")?;
 
         info!("Executing CEF subprocess ..");
 
+        // Execute the CEF subprocess.
         let ret = cef_execute_process(main_args.as_raw(), null_mut(), null_mut()) as i32;
 
         info!("CEF exited with code: {}", ret);
 
+        // Close the CEF framework.
         lib.close()?;
 
         info!("Closed CEF library.");
