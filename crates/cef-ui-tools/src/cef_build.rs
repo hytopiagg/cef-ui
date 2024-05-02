e8d3f734faeef3931e968683d59ed42608cb4f23
wusticality <1028792+wusticality@users.noreply.github.com>
2024-05-02 14:25:24 -0600

    Code to build mac app bundles

--- crates/cef-ui-tools/src/cef_build.rs
+++ crates/cef-ui-tools/src/cef_build.rs
@@ -1,8 +1,22 @@
 use anyhow::Result;
-use cef_ui_util::cef_build;
+use cef_ui_util::{get_tool_workspace_dir, AppBundleSettings, BuildSettings};
 
 fn main() -> Result<()> {
-    cef_build()?;
+    let workspace_dir = get_tool_workspace_dir()?;
+
+    BuildSettings {
+        package_main:        String::from("cef-ui-simple"),
+        package_helper:      String::from("cef-ui-simple-helper"),
+        app_bundle_settings: AppBundleSettings {
+            artifacts_dir:   workspace_dir.join("artifacts"),
+            app_name:        String::from("cef-ui-simple"),
+            main_exe_name:   String::from("cef-ui-simple"),
+            helper_exe_name: String::from("cef-ui-simple-helper"),
+            resources_dir:   workspace_dir.join("resources/macos"),
+            org_name:        String::from("hytopia")
+        }
+    }
+    .run()?;
 
     Ok(())
 }
--- crates/cef-ui-util/src/tools/cef_build.rs
+++ crates/cef-ui-util/src/tools/cef_build.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use clap::Parser;
-use std::process::{Command, Stdio};
 use tracing::{info, level_filters::LevelFilter, subscriber::set_global_default, Level};
 use tracing_log::LogTracer;
 use tracing_subscriber::FmtSubscriber;
+
+use crate::{build_exe, AppBundleSettings};
 
 /// Command line arguments.
 #[derive(Parser, Default)]
@@ -13,51 +14,49 @@ struct BuildArgs {
     pub profile: String
 }
 
-/// Build the project.
-pub fn cef_build() -> Result<()> {
-    // This routes log macros through tracing.
-    LogTracer::init()?;
+pub struct BuildSettings {
+    /// The name of the main package.
+    pub package_main: String,
 
-    // Setup the tracing subscriber globally.
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(LevelFilter::from_level(Level::INFO))
-        .finish();
+    /// The name of the helper package.
+    pub package_helper: String,
 
-    set_global_default(subscriber)?;
-
-    info!("Building main binary ..");
-
-    let args = BuildArgs::parse();
-
-    // Build the main executable.
-    build_exe("cef-ui-simple", &args.profile)?;
-
-    // On macOS, build the helper executable
-    // and package the app bundle as required.
-    if cfg!(target_os = "macos") {
-        use crate::builds::build_app_bundle;
-
-        info!("Building helper binary ..");
-
-        build_exe("cef-ui-simple-helper", &args.profile)?;
-
-        info!("Building app bundle ..");
-
-        build_app_bundle(&args.profile)?;
-    }
-
-    Ok(())
+    /// The app bundle settings.
+    pub app_bundle_settings: AppBundleSettings
 }
 
-/// Build a specific executable.
-pub fn build_exe(name: &str, profile: &str) -> Result<()> {
-    let args = vec!["build", "--bin", name, "--profile", profile];
+impl BuildSettings {
+    pub fn run(&self) -> Result<()> {
+        // This routes log macros through tracing.
+        LogTracer::init()?;
 
-    Command::new("cargo")
-        .args(&args)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()?;
+        // Setup the tracing subscriber globally.
+        let subscriber = FmtSubscriber::builder()
+            .with_max_level(LevelFilter::from_level(Level::INFO))
+            .finish();
 
-    Ok(())
+        set_global_default(subscriber)?;
+
+        info!("Building main binary ..");
+
+        let args = BuildArgs::parse();
+
+        // Build the main executable.
+        build_exe(&self.package_main, &args.profile)?;
+
+        // On macOS, build the helper executable
+        // and package the app bundle as required.
+        if cfg!(target_os = "macos") {
+            info!("Building helper binary ..");
+
+            build_exe(&self.package_helper, &args.profile)?;
+
+            info!("Building app bundle ..");
+
+            self.app_bundle_settings
+                .run(&args.profile)?;
+        }
+
+        Ok(())
+    }
 }
