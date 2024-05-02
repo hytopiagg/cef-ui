5d8d183895f0879849a7f3b8e639dc41475e6e1d
wusticality <1028792+wusticality@users.noreply.github.com>
2024-05-02 10:56:40 -0600

    Fix compilation issues

--- crates/cef-ui-util/src/builds/mod.rs
+++ crates/cef-ui-util/src/builds/mod.rs
@@ -1,17 +1,11 @@
 mod get_cef;
 mod link_cef;
 mod link_cef_helper;
+mod macos;
 mod paths;
 
 pub use get_cef::*;
 pub use link_cef::*;
 pub use link_cef_helper::*;
-pub use paths::*;
-
-// These are macOS-specific.
-#[cfg(all(target_os = "macos"))]
-mod macos;
-
-// These are macOS-specific.
-#[cfg(all(target_os = "macos"))]
 pub use macos::*;
+pub use paths::*;
